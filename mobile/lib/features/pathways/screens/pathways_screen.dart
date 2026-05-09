e26c37047729adbc20642ebbe11319a89f5cbfa5
John Zachary N. Gillana <jzekken@users.noreply.github.com>
2026-05-10 00:11:48 +0800

    fix(pathways ui): add dynamic bottom padding to prevent navbar overlap

--- mobile/lib/features/pathways/screens/pathways_screen.dart
+++ mobile/lib/features/pathways/screens/pathways_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import '../../../core/widgets/gradient_scaffold.dart'; // Adjust path if needed
-
+import 'package:tuklascope_mobile/core/navigation/main_nav_scope.dart';
 import '../data/mock_pathways_data.dart';
 import '../widgets/header_section.dart';
 import '../widgets/project_card.dart';
@@ -11,6 +11,8 @@ class PathwaysScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final navScope = MainNavScope.maybeOf(context);
+    final isNavBarVisible = navScope?.isNavBarVisible ?? true;
     return PopScope(
       canPop: true,
       onPopInvokedWithResult: (didPop, result) {
@@ -19,8 +21,17 @@ class PathwaysScreen extends StatelessWidget {
       child: GradientScaffold(
         body: Center(
           child: ListView.builder(
-            itemCount: 1 + myProjects.length,
+            itemCount: 2 + myProjects.length,
             itemBuilder: (context, index) {
+
+              if (index == 1 + myProjects.length) {
+                return AnimatedContainer(
+                  duration: const Duration(milliseconds: 400),
+                  curve: Curves.easeOutQuint,
+                  height: (isNavBarVisible ? 100.0 : 20.0) + MediaQuery.paddingOf(context).bottom,
+                );
+              }
+              
               Widget item;
               
               if (index == 0) {
